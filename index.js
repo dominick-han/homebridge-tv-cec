358dd060caecb69ad7e50ede11b29f07bda9e59a
Dominick Han <dominick.han@mail.utoronto.ca>
2018-02-20 22:43:29 -0500

    Fixed automation

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let Service, Characteristic;
 const { spawn } = require('child_process');
 const cec_client = spawn('cec-client', ['-d', '8']);
-let cec_callback = null;
+let cec_callback = null, on = false;
 
 cec_client.stdout.on('data', function(data) {
 	data = data.toString();
 	if (data.indexOf('>> 01:46') !== -1) {
+		on = true;
 		cec_client.stdin.write('tx 10:47:52:50:69'); // Set OSD String to 'RPi'
 	} else if (data.indexOf('>> 0f:36') !== -1 || data.indexOf('>> 01:90:00') !== -1) {
 		if (cec_callback) {
@@ -61,10 +62,12 @@ TVPower.prototype = {
 	getState: function(callback) {
 		cec_client.stdin.write('tx 10:8f'); // 'pow 0'
 		cec_callback = function () {
+			on = true;
 			callback(null, true);
 		};
 		setTimeout(function () {
 			if (cec_callback !== null) {
+				on = false;
 				cec_callback = null;
 				callback(null, false);
 			}
@@ -72,11 +75,18 @@ TVPower.prototype = {
 	},
 
 	setState: function(state, callback) {
-		if (state) {
-			cec_client.stdin.write('tx 10:04'); // 'pow 0'
+		if (state === on) {
+			callback();
 		} else {
-			cec_client.stdin.write('tx 10:36'); // 'pow 0'
+			if (state) {
+				cec_client.stdin.write('tx 10:04'); // 'on 0'
+			} else {
+				cec_client.stdin.write('tx 10:36'); // 'standby 0'
+			}
+			cec_callback = function () {
+				on = state;
+				callback();
+			};
 		}
-		cec_callback = callback;
 	}
 };
