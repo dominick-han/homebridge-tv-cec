5f353a5fa9a76d2bae683a007cbf1b8f604e0164
Dominick Han <dominick.han@mail.utoronto.ca>
2018-02-20 23:07:59 -0500

    v0.2.1 - Updated architecture

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let Service, Characteristic;
 const { spawn } = require('child_process');
 const cec_client = spawn('cec-client', ['-d', '8']);
-let cec_callback = null, on = false;
+let cec_callback = null, powerSwitch = null;
 
 cec_client.stdout.on('data', function(data) {
 	data = data.toString();
 	if (data.indexOf('>> 01:46') !== -1) {
-		on = true;
+		powerSwitch.setCharacteristic(Characteristic.On, true);
 		cec_client.stdin.write('tx 10:47:52:50:69'); // Set OSD String to 'RPi'
 	} else if (data.indexOf('>> 0f:36') !== -1 || data.indexOf('>> 01:90:00') !== -1) {
 		if (cec_callback) {
@@ -49,25 +49,23 @@ TVPower.prototype = {
 			.setCharacteristic(Characteristic.Model, this.config.model || 'TV')
 			.setCharacteristic(Characteristic.SerialNumber, this.config.serial || 'N/A');
 
-		this.switchService = new Service.Switch(this.config.name);
-		this.switchService
+		powerSwitch = new Service.Switch(this.config.name);
+		powerSwitch
 			.getCharacteristic(Characteristic.On)
 			.on('get', this.getState.bind(this))
 			.on('set', this.setState.bind(this));
 		this.log(`Initialized ${this.config.name || 'TV'}`);
 
-		return [this.informationService, this.switchService];
+		return [this.informationService, powerSwitch];
 	},
 
 	getState: function(callback) {
 		cec_client.stdin.write('tx 10:8f'); // 'pow 0'
 		cec_callback = function () {
-			on = true;
 			callback(null, true);
 		};
 		setTimeout(function () {
 			if (cec_callback !== null) {
-				on = false;
 				cec_callback = null;
 				callback(null, false);
 			}
@@ -75,7 +73,7 @@ TVPower.prototype = {
 	},
 
 	setState: function(state, callback) {
-		if (state === on) {
+		if (state === powerSwitch.getCharacteristic(Characteristic.On)) {
 			callback();
 		} else {
 			if (state) {
@@ -84,7 +82,7 @@ TVPower.prototype = {
 				cec_client.stdin.write('tx 10:36'); // 'standby 0'
 			}
 			cec_callback = function () {
-				on = state;
+				powerSwitch.setCharacteristic(Characteristic.On, state);
 				callback();
 			};
 		}
