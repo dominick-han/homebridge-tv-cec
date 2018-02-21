8a55cd3ea68cfa8a926fa2a109b78d4276a0ebb9
Dominick Han <dominick.han@mail.utoronto.ca>
2018-02-20 21:07:49 -0500

    Got power status working

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 let Service, Characteristic;
+const { spawn } = require('child_process');
+const cec_client = spawn('cec-client', ['-d', '8']);
+let cec_output = [];
+
+cec_client.stdout.on('data', function(data) {
+	cec_output.push(data.toString());
+});
 
 module.exports = function(homebridge) {
 	Service = homebridge.hap.Service;
@@ -34,9 +41,9 @@ TVPower.prototype = {
 
 		this.switchService = new Service.Switch(this.config.name);
 		this.switchService
-                .getCharacteristic(Characteristic.On)
-                .on('get', this.getState.bind(this))
-                .on('set', this.setState.bind(this));
+			.getCharacteristic(Characteristic.On)
+			.on('get', this.getState.bind(this))
+			.on('set', this.setState.bind(this));
 		this.log(`Initialized ${this.config.name || 'TV'}`);
 
 		return [this.informationService, this.switchService];
@@ -44,7 +51,17 @@ TVPower.prototype = {
 
 	getState: function(callback) {
 		this.log("getState()");
-		callback(null, true);
+		cec_output = [];
+		cec_client.stdin.write("tx 10:8f");
+		setTimeout(function () {
+			let success = false;
+			for (let i in cec_output) {
+				if (cec_output[i].indexOf("01:90:00") !== -1) {
+					success = true;
+				}
+			}
+			callback(null, success);
+		}, 300);
 	},
 
 	setState: function(on, callback) {
