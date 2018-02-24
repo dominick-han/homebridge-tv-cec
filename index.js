10817bd135952ee81e8224bef37f4f461a051812
Dominick Han <dominick.han@mail.utoronto.ca>
2018-02-24 12:15:29 -0500

    Deleted setCharacteristic (wrong use)

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ let cec_callback = null, powerSwitch = null;
 cec_client.stdout.on('data', function(data) {
 	data = data.toString();
 	if (data.indexOf('<< 10:47') !== -1) {
-		powerSwitch.setCharacteristic(Characteristic.On, true);
 		cec_client.stdin.write('tx 10:47:52:50:69'); // Set OSD String to 'RPi'
 	} else if (data.indexOf('>> 0f:36') !== -1 || data.indexOf('>> 01:90:00') !== -1) {
 		if (cec_callback) {
@@ -82,7 +81,6 @@ TVPower.prototype = {
 				cec_client.stdin.write('tx 10:36'); // 'standby 0'
 			}
 			cec_callback = function () {
-				powerSwitch.setCharacteristic(Characteristic.On, state);
 				callback(null, state);
 			};
 		}
